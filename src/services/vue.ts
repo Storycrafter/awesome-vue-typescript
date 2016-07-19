a06acbfd3d83120bf7c8256319dc1607afc26402
Daniel Schmitz <danieljfa@gmail.com>
2016-07-19 10:55:42 -0300

    Fix resource html template table

--- src/services/vue.ts
+++ src/services/vue.ts
@@ -1,5 +1,8 @@
 
 export class VueService{
+    /**
+     * Get issues from Vuejs repository
+     */
     static issues():PromiseLike<vuejs.HttpResponse>{
         return Vue.http.get('https://api.github.com/repos/vuejs/vue/issues')
     }
